945eeee92cc5a9367e6ca68b518bf826ca3338ef
JosephGakah <joegakah@gmail.com>
2023-03-20 23:20:36 +0300

    create logo page

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import { preview } from '../assets'
+import getRandomPrompt from '../utils'
+import { FormField, Loader } from '../components'
 
 const CreatePost = () => {
+
+  
   return (
     <div>CreatePost</div>
   )
