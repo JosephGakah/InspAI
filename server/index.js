d46751d9a01f2f7510c4d572e3e63008ecaa0b5c
JosephGakah <joegakah@gmail.com>
2023-03-24 23:59:39 +0300

    routes

--- server/index.js
+++ server/index.js
@@ -1,12 +1,19 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
+
 import connectDb from './mongodb/connect.js';
+import postRoutes from './routes/postRoutes.js';
+import midtrekRoutes from './routes/midtrekRoutes.js';
+
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
+
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/midtrek', midtrekRoutes);
 
 app.get('/', async (req, res) => {
     res.send('Hi There Designer!')
