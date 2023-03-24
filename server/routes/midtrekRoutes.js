0dbe5c0bb09a51e30943b81adc42f0ee22a88cef
JosephGakah <joegakah@gmail.com>
2023-03-25 00:27:58 +0300

    openai api

--- server/routes/midtrekRoutes.js
+++ server/routes/midtrekRoutes.js
@@ -5,3 +5,32 @@ import { Configuration, OpenAIApi } from 'openai';
 dotenv.config();
 
 const router = express.Router();
+
+const config = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY
+})
+
+const openai = new OpenAIApi(config)
+
+router.route('/').get((req, res) => {
+    res.send('Hello from Dall-e')
+    console.log('DALL-E')
+})
+
+router.route('/').get(async (req, res) =>{
+    try {
+        const prompt = req.body;
+        const aiRespose = await openai.createImage({
+            prompt,
+            n: 1,
+            size: '1024x1024',
+            response_format: 'b64_json'
+        })
+        const image = aiRespose.data.data[0].b64_json;
+        res.status(400).json({image: image})        
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error?.response.data.error.message)
+    }
+})
+export default router;
